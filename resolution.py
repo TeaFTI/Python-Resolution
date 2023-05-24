6fec76212640d7e91248a3fd2c3a033de25fbe32
Yan Kuang <YTKme@Outlook.com>
2023-05-24 00:22:10 -0700

    Add project file

---
+++ resolution.py
@@ -0,0 +1,24 @@
+"""
+Resolution
+~~~~~~~~~~
+"""
+
+RESOLUTION_FILENAME = 'resolution.json'
+
+ATTRIBUTE_LIST = []
+
+def __getattribute__(attribute):
+    """"""
+    return attribute
+
+def main():
+    """Main Function
+
+    The Main Function, also the entry point for the console script, or
+    CLI (Command Line Interface)
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main()
